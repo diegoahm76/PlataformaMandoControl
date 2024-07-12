32c28d05d2912b575263f81e68c4d37129ba6f55
Oscar Steven Rodríguez Galeano <oscarstep.123@gmail.com>
2024-07-12 07:54:06 -0500

    Feature: Add service for geojson inscripcion dga

--- geojson/urls/geojson_urls.py
+++ geojson/urls/geojson_urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from geojson.views import geojson_estaciones_views as estaciones_views
 from geojson.views import geojson_tramites_views as tramites_views
+from geojson.views import geojson_opas_views as opas_views
 
 urlpatterns=[
     #Estaciones
 
     #Tramites
     path('tramite/conc_aguas/get/',tramites_views.GeoJsonConcesionAguasSuperficialesView.as_view(),name='geojson-concesion-aguas'),
+
+    #Opas
+    path('opa/inscripcion_dga/get/',opas_views.GeoJsonInscripcionDGAView.as_view(),name='geojson-inscripcion-dga'),
 ]
